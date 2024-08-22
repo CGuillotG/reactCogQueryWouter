d9ef9c63fbf73a574ebc8ca91b70b59acb199d3c
CGuillotG <carlos@guillot.dev>
2024-08-22 00:55:51 -0600

    Created Auth and Sign wrappers

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './components/Home.jsx';
-import SignIn from './components/SignIn.jsx'
-import SignUp from './components/SignUp.jsx'
-import ConfirmSignUp from './components/ConfirmSignUp.jsx'
+import SignIn from './components/SignIn.jsx';
+import SignUp from './components/SignUp.jsx';
+import ConfirmSignUp from './components/ConfirmSignUp.jsx';
 
-function App() {
-  const isAuthenticated = () => {
-    const accessToken = sessionStorage.getItem('accessToken');
-    return !!accessToken;
-  };
-
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={isAuthenticated() ? <Navigate replace to="/home" /> : <Navigate to="/signin" replace />} />
-          <Route path="/home" element={isAuthenticated() ? <Home /> : <Navigate replace to="/signin" />} />
-          <Route path="/signin" element={!isAuthenticated() ? <SignIn /> : <Navigate to="/" replace />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/confirm" element={<ConfirmSignUp />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+function AuthRoute({ component: Component }) {
+    const isAuth = () => {
+        const accessToken = sessionStorage.getItem('accessToken');
+        return !!accessToken;
+    };
+    return isAuth() ? <Component /> : <Navigate to="/signin" replace />;
 }
 
-export default App
+function SignRoute({ component: Component }) {
+    const isAuth = () => {
+        const accessToken = sessionStorage.getItem('accessToken');
+        return !!accessToken;
+    };
+    return isAuth() ? <Navigate to="/home" replace /> : <Component />;
+}
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/home" replace />} />
+                    <Route path="/home" element={<AuthRoute component={Home} />} />
+                    <Route path="/signin" element={<SignRoute component={SignIn} />} />
+                    <Route path="/signup" element={<SignRoute component={SignUp} />} />
+                    <Route path="/confirm" element={<SignRoute component={ConfirmSignUp} />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
