ca3ad6d6ba9ecaa7d0962e553b6e62f847b7e782
CGuillotG <carlos@guillot.dev>
2024-09-23 13:23:40 -0600

    Added React Query Eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tanStackQuery from '@tanstack/eslint-plugin-query'
 
 export default [
   { ignores: ['dist'] },
@@ -35,4 +36,5 @@ export default [
       ],
     },
   },
+  ...tanStackQuery.configs['flat/recommended']
 ]
